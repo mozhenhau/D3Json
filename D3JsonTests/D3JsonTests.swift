6166dfb768a649366ef674b5087ad6dd8baf9213
mozhenhau <493842062@qq.com>
2015-05-15 11:45:30 +0800

    change to framework

--- D3JsonTests/D3JsonTests.swift
+++ D3JsonTests/D3JsonTests.swift
@@ -2,7 +2,7 @@
 //  D3JsonTests.swift
 //  D3JsonTests
 //
-//  Created by mozhenhau on 15/5/15.
+//  Created by mozhenhau on 15/2/12.
 //  Copyright (c) 2015年 mozhenhau. All rights reserved.
 //
 
@@ -13,24 +13,57 @@ class D3JsonTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        textExampleJsonList()
+        // ps : if you want to test , you should delete the annotation in D3Json.swift at line 136-140
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        var json = [
+            "name": "ok",
+            "age":1,
+            "job": [
+                "name":"swift"
+            ]
+        ]
+        var user:User = D3Json.jsonToModel(json, objc: User())
+        println(user)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
+    func textExampleJsonList(){
+        var json2 = [
+            [
+                "name": "ok",
+                "age":1,
+                "job": [
+                    "name":"swift"
+                ]
+            ],
+            [
+                "name": "no ok",
+                "age":2,
+                "job": [
+                    "name":"obc"
+                ]
+            ]
+        ]
+        var users:Array<User> = D3Json.jsonToModelList(json2, objc: User())
+        println(users)
+    }
+}
+
+@objc(Job)
+class Job:NSObject{
+    var name = ""
+}
+
+@objc(User)
+class User:NSObject{
+    var name = ""
+    var age = 0
+    var job:Job = Job()
 }
